c589431909a6726c16a02c39305327454f1645b1
kazarus <kazarus@qq.com>
2019-09-04 19:54:59 +0800

    20190904

---
+++ WsEngine.go
@@ -0,0 +1,15 @@
+package WsEngine
+
+import "github.com/gorilla/websocket"
+
+
+type TWsEngine struct {
+	WsClient map[*websocket.Conn]string
+	WsNotify chan TWsNotify
+}
+
+type TWsNotify struct {
+	EntityID string `json:"entityid"`
+	UserName string `json:"username"`
+	Notified string `json:"notified"`
+}
